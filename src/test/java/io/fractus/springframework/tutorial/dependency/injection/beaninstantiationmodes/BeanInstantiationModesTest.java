ebd8e42d40279a6d2ad6092f9a71048aa730a240
dstar55 <dstar55@yahoo.com>
2017-04-26 11:15:00 +0200

    #8 initial commit

---
+++ src/test/java/io/fractus/springframework/tutorial/dependency/injection/beaninstantiationmodes/BeanInstantiationModesTest.java
@@ -0,0 +1,33 @@
+package io.fractus.springframework.tutorial.dependency.injection.beaninstantiationmodes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class BeanInstantiationModesTest {
+
+	private static BeanFactory BEAN_FACTORY;
+	
+	@BeforeClass
+	public static void before(){
+		BEAN_FACTORY = new ClassPathXmlApplicationContext("beaninstantiationmodes-context.xml");
+	}
+	
+	@Test
+	public void testBeanbeanInstantiationModesTest() {
+		
+        String firstSingleton = (String)BEAN_FACTORY.getBean("singleton");
+        String secondSingleton = (String)BEAN_FACTORY.getBean("singleton");
+        
+        assertEquals(true, (firstSingleton == secondSingleton));
+                
+        String firstPrototype = (String)BEAN_FACTORY.getBean("prototype");
+        String secondPrototype = (String)BEAN_FACTORY.getBean("prototype");
+        
+        assertEquals(false, (firstPrototype == secondPrototype));
+	}
+}
